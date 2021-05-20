cb2c223e782be227b4e7b92460473a50657244f0
Jure Vidmar <vidmarjur@gmail.com>
2021-05-20 15:15:40 +0000

    Cleanup core installation part, add chroot stuff to a single function

---
+++ install_core.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+# Custom Arch linux system installation script
+# Based on https://disconnected.systems/blog/archlinux-installer/ 
+# by Jure Vidmar
+
+
+# Update pacman + choose fastest pacman mirrors
+pacman -Sy --needed --noconfirm pacman-contrib dialog git
+echo "Updating mirror list"
+MIRRORLIST_URL="https://archlinux.org/mirrorlist/?country=SI&country=NL&protocol=http&protocol=https&ip_version=4"
+curl -s "$MIRRORLIST_URL" | \
+    sed -e 's/^#Server/Server/' -e '/^#/d' | \
+    rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
+
+
+# Get user input: hostname, username, password
+hostname=$(dialog --stdout --inputbox "Enter hostname" 0 0) || exit 1
+clear
+: ${hostname:?"hostname cannot be empty"}
+
+user=$(dialog --stdout --inputbox "Enter admin username" 0 0) || exit 1
+clear
+: ${user:?"user cannot be empty"}
+
+password=$(dialog --stdout --passwordbox "Enter admin password" 0 0) || exit 1
+clear
+: ${password:?"password cannot be empty"}
+password2=$(dialog --stdout --passwordbox "Enter admin password again" 0 0) || exit 1
+clear
+[[ "$password" == "$password2" ]] || ( echo "Passwords did not match"; exit 1; )
+
+
+timedatectl set-ntp true
+# setup logging
+exec 1> >(tee "install_script_stdout.log")
+exec 2> >(tee "install_script_stderr.log")
+
+
+# Disk partitioning and filesystems
+devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)
+device=$(dialog --stdout --menu "Select installtion disk" 0 0 0 ${devicelist}) || exit 1
+clear
+
+boot_size=513Mib    # 512 + 1MiB offset. WHy do we need that? MBR?
+parted --script "${device}" -- \
+  mklabel gpt \
+  mkpart ESP fat32 1MiB ${boot_size} \
+  set 1 boot on \
+  mkpart primary ext4 ${boot_size} 100%
+
+part_boot="$(ls ${device}* | grep -E "^${device}p?1$")"
+part_root="$(ls ${device}* | grep -E "^${device}p?2$")"
+
+wipefs "${part_boot}" && mkfs.vfat -F32 "${part_boot}"
+wipefs "${part_root}" && mkfs.ext4 "${part_root}"
+
+mount "${part_root}" /mnt
+mkdir /mnt/boot
+mount "${part_boot}" /mnt/boot
+
+
+# Install base system
+pacstrap /mnt linux linux-firmware base base-devel sudo man-db man-pages nano openssh parted wpa_supplicant networkmanager
+
+
+# Post-installation stuff
+genfstab -t PARTUUID /mnt > /mnt/etc/fstab
+chroot_stuff () {
+    
+    # Hostname
+    printf "$hostname\n" > /etc/hostname
+
+    # Locale
+    locale="en_US.UTF-8"
+    printf "LANG=$locale\n" > /etc/locale.conf
+    sed -i "/$locale/s/^#//" /etc/locale.gen
+    locale-gen
+
+    # Create user account
+    useradd -mU -G wheel,video,audio,storage,lp,optical "$user"
+    printf "$user:$password\n" | chpasswd
+    printf "%%wheel ALL=(ALL) ALL\n" > /etc/sudoers.d/99_wheel
+    visudo -cf /etc/sudoers.d/99_wheel
+
+    # Boot manager (systemd-boot)
+    bootctl install
+    printf "default archlinux\n" > /boot/loader.conf
+    cat <<EOF > /boot/loader/entries/archlinux.conf
+title    Arch Linux
+linux    /vmlinuz-linux
+initrd   /initramfs-linux.img
+options  root=PARTUUID=$(blkid -s PARTUUID -o value "$part_root") rw
+EOF
+
+}
+export -f chroot_stuff
+arch-chroot /mnt /bin/bash -c "chroot_stuff" || exit 1
+
+printf "\n\nCore system installed successfully\n"

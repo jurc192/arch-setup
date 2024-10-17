49c60a7f67ed06abad8e5ea8599efa1216331d28
Jure Vidmar <vidmarjur@gmail.com>
2024-10-17 22:47:23 +0200

    Debug heredoc issues

--- install_userspace.sh
+++ install_userspace.sh
@@ -10,38 +10,36 @@ user=$1
 # Install userspace
 arch-chroot /mnt /bin/bash <<EOF
 
-    echo "Bulding and installing jur-userspace"
-    runuser "$user" git clone https://github.com/jurc192/arch-packages
-    cd arch-packages/jur-userspace
+    echo "Building and installing jur-userspace"
+    git clone https://github.com/jurc192/arch-packages /home/$user/arch-packages
+    cd /home/$user/arch-packages/jur-userspace
     makepkg -si
 
-    runuser $user <<- 'HEREDOC2'
-        cd /home/"$user"/
+    # Run user-specific commands using runuser with heredoc
+    runuser "$user" << 'HEREDOC2'
+        cd /home/$USER/
 
         DIR_NAME=".dotfiles_git"
-        git clone --bare https://github.com/jurc192/dotfiles.git \$HOME/\$DIR_NAME || exit 1
+        git clone --bare https://github.com/jurc192/dotfiles.git $HOME/$DIR_NAME || exit 1
 
         function dotfiles {
-        /usr/bin/git --git-dir=\$HOME/\$DIR_NAME --work-tree=\$HOME \$@
+            /usr/bin/git --git-dir=$HOME/$DIR_NAME --work-tree=$HOME "$@"
         }
+
         dotfiles checkout
-        if [ \$? -ne 0 ]; then
-            # if checkout fails, remove all files
+        if [ $? -ne 0 ]; then
             printf "Removing conflicting files due to failed checkout\n"
-            cd \$HOME
-            dotfiles checkout 2>&1 | grep -E "\s+\." | awk '{\$1=\$1;print}' | xargs -d '\n' rm -rf
+            cd $HOME
+            dotfiles checkout 2>&1 | grep -E "\s+\." | awk '{$1=$1;print}' | xargs -d '\n' rm -rf
             dotfiles checkout || exit 1
             printf "Dotfiles checkout succeeded\n"
-        fi;
+        fi
 
         dotfiles config status.showUntrackedFiles no
-        
         printf "\nConfiguration files applied successfully\n"
 
         # Install themes
-        git clone https://github.com/jurc192/themes.git \$HOME/.themes || exit 1
+        git clone https://github.com/jurc192/themes.git $HOME/.themes || exit 1
     HEREDOC2
 
-
-    printf "\n\nUserspace installed successfully\n\n"
 EOF

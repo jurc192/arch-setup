031493d4aafa0abd1050f1a8def6296bb0e07bbd
Jure Vidmar <vidmarjur@gmail.com>
2024-10-17 22:53:03 +0200

    Replace HEREDOC with a -c invocation at runuser command

--- install_jur_system.sh
+++ install_jur_system.sh
@@ -106,6 +106,9 @@ CHROOT
 printf "\n\nCore system installed successfully\n\n"
 
 # Install userspace
+printf "\n\nCore system installed successfully\n\n"
+
+# Install userspace
 arch-chroot /mnt /bin/bash <<EOF
 
     echo "Building and installing jur-userspace"
@@ -113,32 +116,33 @@ arch-chroot /mnt /bin/bash <<EOF
     cd /home/$user/arch-packages/jur-userspace
     makepkg -si
 
-    # Run user-specific commands using runuser with heredoc
-    runuser "$user" << 'HEREDOC2'
-        cd /home/$USER/
+    # Run all user-specific commands using runuser -c ""
+    runuser "$user" -c "
+        cd /home/$user/
 
-        DIR_NAME=".dotfiles_git"
-        git clone --bare https://github.com/jurc192/dotfiles.git $HOME/$DIR_NAME || exit 1
+        DIR_NAME='.dotfiles_git'
+        git clone --bare https://github.com/jurc192/dotfiles.git \$HOME/\$DIR_NAME || exit 1
 
         function dotfiles {
-            /usr/bin/git --git-dir=$HOME/$DIR_NAME --work-tree=$HOME "$@"
+            /usr/bin/git --git-dir=\$HOME/\$DIR_NAME --work-tree=\$HOME \"\$@\"
         }
 
         dotfiles checkout
-        if [ $? -ne 0 ]; then
-            printf "Removing conflicting files due to failed checkout\n"
-            cd $HOME
-            dotfiles checkout 2>&1 | grep -E "\s+\." | awk '{$1=$1;print}' | xargs -d '\n' rm -rf
+        if [ \$? -ne 0 ]; then
+            # If checkout fails, remove all conflicting files
+            printf 'Removing conflicting files due to failed checkout\n'
+            cd \$HOME
+            dotfiles checkout 2>&1 | grep -E '\s+\.' | awk '{\$1=\$1;print}' | xargs -d '\n' rm -rf
             dotfiles checkout || exit 1
-            printf "Dotfiles checkout succeeded\n"
+            printf 'Dotfiles checkout succeeded\n'
         fi
 
         dotfiles config status.showUntrackedFiles no
-        printf "\nConfiguration files applied successfully\n"
+        printf '\nConfiguration files applied successfully\n'
 
         # Install themes
-        git clone https://github.com/jurc192/themes.git $HOME/.themes || exit 1
-    HEREDOC2
+        git clone https://github.com/jurc192/themes.git \$HOME/.themes || exit 1
+    "
 
 EOF
 
--- install_userspace.sh
+++ install_userspace.sh
@@ -8,6 +8,9 @@ fi;
 user=$1
 
 # Install userspace
+printf "\n\nCore system installed successfully\n\n"
+
+# Install userspace
 arch-chroot /mnt /bin/bash <<EOF
 
     echo "Building and installing jur-userspace"
@@ -15,31 +18,34 @@ arch-chroot /mnt /bin/bash <<EOF
     cd /home/$user/arch-packages/jur-userspace
     makepkg -si
 
-    # Run user-specific commands using runuser with heredoc
-    runuser "$user" << 'HEREDOC2'
-        cd /home/$USER/
+    # Run all user-specific commands using runuser -c ""
+    runuser "$user" -c "
+        cd /home/$user/
 
-        DIR_NAME=".dotfiles_git"
-        git clone --bare https://github.com/jurc192/dotfiles.git $HOME/$DIR_NAME || exit 1
+        DIR_NAME='.dotfiles_git'
+        git clone --bare https://github.com/jurc192/dotfiles.git \$HOME/\$DIR_NAME || exit 1
 
         function dotfiles {
-            /usr/bin/git --git-dir=$HOME/$DIR_NAME --work-tree=$HOME "$@"
+            /usr/bin/git --git-dir=\$HOME/\$DIR_NAME --work-tree=\$HOME \"\$@\"
         }
 
         dotfiles checkout
-        if [ $? -ne 0 ]; then
-            printf "Removing conflicting files due to failed checkout\n"
-            cd $HOME
-            dotfiles checkout 2>&1 | grep -E "\s+\." | awk '{$1=$1;print}' | xargs -d '\n' rm -rf
+        if [ \$? -ne 0 ]; then
+            # If checkout fails, remove all conflicting files
+            printf 'Removing conflicting files due to failed checkout\n'
+            cd \$HOME
+            dotfiles checkout 2>&1 | grep -E '\s+\.' | awk '{\$1=\$1;print}' | xargs -d '\n' rm -rf
             dotfiles checkout || exit 1
-            printf "Dotfiles checkout succeeded\n"
+            printf 'Dotfiles checkout succeeded\n'
         fi
 
         dotfiles config status.showUntrackedFiles no
-        printf "\nConfiguration files applied successfully\n"
+        printf '\nConfiguration files applied successfully\n'
 
         # Install themes
-        git clone https://github.com/jurc192/themes.git $HOME/.themes || exit 1
-    HEREDOC2
+        git clone https://github.com/jurc192/themes.git \$HOME/.themes || exit 1
+    "
 
 EOF
+
+printf "\n\nUserspace installed successfully\n\n"
